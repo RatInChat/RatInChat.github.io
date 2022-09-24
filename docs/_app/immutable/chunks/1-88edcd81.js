229b773a6bd6674033c8fd1a48906f2a09426b21
RatInChat <jbarth632@gmail.com>
2022-09-24 15:07:04 -0500

    add /docs

--- docs/_app/immutable/chunks/1-88edcd81.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-cae1c32b.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-f9007a6a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ce4db10a.js";export{t as component};
