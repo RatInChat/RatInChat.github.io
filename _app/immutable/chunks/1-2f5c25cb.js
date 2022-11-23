7f66d6ec92528c31fe5063342ce75f888eccb786
RatInChat <98173006+RatInChat@users.noreply.github.com>
2022-11-22 18:10:16 -0600

    Delete _app directory

--- _app/immutable/chunks/1-2f5c25cb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-b1a79d31.js";export{t as component};
--- _app/immutable/chunks/2-782cb606.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-44e06313.js";export{t as component};
